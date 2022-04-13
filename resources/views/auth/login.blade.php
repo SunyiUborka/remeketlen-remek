2c691e3131c82f20473d7342aba5bbba8bfa6a2f
SunyiUborka <csehi.rolcsi@gmail.com>
2022-04-13 13:20:45 +0200

    login fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,33 +2,31 @@
 
 @section('content')
     <div class="panel">
-    {!! Form::open(['route' => 'auth.authenticate', "class" =>"is-invalid"]) !!}
-        <h1>Bejelentkezés</h1>
-        <div class="form-item">
-            {{Form::label('username','Felhasználónév')}}
-            {{Form::text('username', $value = old('username'), $attributes = ["class"=>"form-control"])}}
-        </div>
-        <div class="form-item">
-            {{Form::label('password',"Jelszó")}}
-
-            @error('password')
-            {{Form::password('password', ['class' => 'form-control is-invalid'])}}
-            <div id="passwordFeedback" class="invalid-feedback">
-                {{ $message }}
+        {!! Form::open(['route' => 'auth.authenticate', "class" =>"is-invalid"]) !!}
+            <h1>Bejelentkezés</h1>
+            <div class="form-item">
+                {{Form::label('username','Felhasználónév')}}
+                {{Form::text('username', $value = old('username'), $attributes = ["class"=>"form-control"])}}
             </div>
-            @else
-                {{Form::password('password', ['class' => 'form-control'])}}
+            <div class="form-item">
+                {{Form::label('password',"Jelszó")}}
+                @error('password')
+                    {{Form::password('password', ['class' => 'form-control is-invalid'])}}
+                    <div id="passwordFeedback" class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @else
+                    {{Form::password('password', ['class' => 'form-control'])}}
                 @enderror
-    </div>
-        <div class="form-item">
-            {{Form::submit('Bejelentkezés', ['class' => 'btn btn-primary'])}}
-        </div>
-    {!! Form::close() !!}
-
-    @if($errors->any)
-        @foreach($errors->all() as $message)
-            <li style="color:red">{{$message}}</li>
-        @endforeach
-    @endif
+            </div>
+            <div class="form-item">
+                {{Form::submit('Bejelentkezés', ['class' => 'btn btn-primary'])}}
+            </div>
+        {!! Form::close() !!}
+        @if($errors->any)
+            @foreach($errors->all() as $message)
+                <li style="color:red">{{$message}}</li>
+            @endforeach
+        @endif
     </div>
 @endsection

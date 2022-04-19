38ce0e16395225c1bd5627c302c28cd6771d36cd
kiss533 <71214581+kiss533@users.noreply.github.com>
2022-04-19 18:34:57 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\SiteController::class, 'index'])->name('home');
 
+// Oldalak
+
+Route::get('/home', [\App\Http\Controllers\SiteController::class, 'home'])->name('dosearch.home');
+
 // Regisztráció
 
 Route::get("/register", [\App\Http\Controllers\RegisterController::class, "create"])

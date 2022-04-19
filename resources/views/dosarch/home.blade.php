e5d39d561b9430fbb60b5968a8acb013a8569ece
kiss533 <71214581+kiss533@users.noreply.github.com>
2022-04-19 18:36:41 +0200

    oldalbevezeto

--- resources/views/dosarch/home.blade.php
+++ resources/views/dosarch/home.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.main')
 
-@section('')
+@section('content')
 
+<h1>A Dos játékok megtekintése</h1>
+
+<ul>
+
+</ul>
+@endsection
+
+@section('innerjs')
+<script>
+fetch('dosarch.home').then(response => response.json())
+.then(data => {
+
+const list = data.data
+let programs = " ";
+for (let key in list) {
+programs = `<li>${key.name}</li>`;
+
+}
+})
+
+let b = document.querySelector('ul');
+</script>
 @endsection

4d083c9d044f844630d06acab212d0496e18d53f
kiss533 <71214581+kiss533@users.noreply.github.com>
2022-04-19 18:28:52 +0200

    menu update

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -2,10 +2,31 @@
     <div class="nav-item">
         <a href="{{route('home')}}" class="nav-link">Kezdőlap</a>
     </div>
+    @can('create-belep')
+    <div class="nav-item">
+        <a href="{{route('dosearch.home')}}" class="nav-link">Dos Programok</a>
+    </div>
+    @endcan
+
+    
+    @auth
+
+    Üdv {{Illuminate\Support\Facades\Auth::user()->username;}}
+   <!-- <a href class="btn btn-light" type="button">Kijelentkezés</a> -->
+   
+    <form class="form-inline" action="{{route('auth.logout')}}" method="POST">
+   @csrf
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Kijelentkezés</button>
+    </form>
+
+
+    @else
     <div class="nav-item">
         <a href="{{route('auth.login')}}" class="nav-link">Belépés</a>
     </div>
     <div class="nav-item">
         <a href="{{route('register.create')}}" class="nav-link">Regisztráció</a>
     </div>
+
+    @endauth
 </nav>

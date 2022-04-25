8d246598a1cbea1c298de1e3476faa6f7ece2b08
SunyiUborka <csehi.rolcsi@gmail.com>
2022-04-25 10:33:54 +0200

    bugfix

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -6,16 +6,8 @@
         {!! Form::open(['route' => 'register.store', 'class' => "auth-form"]) !!}
             <h1>Regisztráció</h1>
             <div class="form-item">
-<<<<<<< HEAD
                 {{Form::label('username','Felhasználónév', ['class' => 'auth-label'])}}
                 {{Form::text('username', $value = old('username'), $attributes = ["class"=>"auth-input"])}}
-            </div>
-            <div class="form-item">
-                {{Form::label('email','Email', ['class' => 'auth-label'])}}
-                {{Form::email('email', $value = old('email'), $attributes = ["class"=>"auth-input"])}}
-=======
-                {{Form::label('username','Felhasználónév')}}
-                {{Form::text('username', $value = old('username'), $attributes = ["class"=>"form-control"])}}
                 @error('username')
                 <div id="usernameFeedBack" class="invalid-feedback">
                     {{ $message }}
@@ -23,15 +15,13 @@
                 @enderror
             </div>
             <div class="form-item">
-                {{Form::label('email','Email')}}
-
-                {{Form::email('email', $value = old('email'), $attributes = ["class"=>"form-control"])}}
+                {{Form::label('email','Email', ['class' => 'auth-label'])}}
+                {{Form::email('email', $value = old('email'), $attributes = ["class"=>"auth-input"])}}
                 @error('email')
                 <div id="emailFeedBack" class="invalid-feedback">
                     {{ $message }}
                 </div>
                 @enderror
->>>>>>> e895a7cdfc2098f83aeae4725b868ac938e73d56
             </div>
             <div class="form-item">
                 {{Form::label('password',"Jelszó", ['class' => 'auth-label'])}}

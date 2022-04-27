d0a0976809d988dcde658fdf41c8848c3e71467e
SunyiUborka <csehi.rolcsi@gmail.com>
2022-04-27 22:45:04 +0200

    update

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ docker-compose up -d
 docker-compose exec php composer install
 docker-compose exec php php artisan key:generate
 docker-compose exec php php artisan migrate --seed
+docker-compose exec php php artisan storage:link
